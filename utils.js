7903a5f78a09dd3c100c6fa0199a34bfdee2057a
uurclkky <115736823+uurclkky@users.noreply.github.com>
2026-03-07 14:26:43 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T11:26:36.345Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-07T11:26:39.390Z
+Commit: Fix broken links in documentation
