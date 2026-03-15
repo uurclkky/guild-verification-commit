dfe89e996c57591cfd5002357af3c593660bddbe
uurclkky <115736823+uurclkky@users.noreply.github.com>
2026-03-15 13:28:08 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T10:28:05.669Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-15T10:28:06.924Z
+Commit: Patch API endpoint error handling
