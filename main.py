9e4aaa842d3accb1e7d35ca46f391291976544f7
uurclkky <115736823+uurclkky@users.noreply.github.com>
2026-03-17 12:04:40 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T10:27:50.085Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-17T09:04:39.007Z
+Commit: Fix memory leak in event handlers
