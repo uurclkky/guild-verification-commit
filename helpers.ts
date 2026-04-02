db175bb4fc03a9bd02fee96eedaff8958d509b5d
uurclkky <115736823+uurclkky@users.noreply.github.com>
2026-04-02 23:54:55 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T21:34:52.053Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-04-02T20:54:54.122Z
+Commit: Patch API endpoint error handling
