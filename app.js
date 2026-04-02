19df86f9fbbec71f9b19feeb396fcb11b3f85f2d
uurclkky <115736823+uurclkky@users.noreply.github.com>
2026-04-02 23:55:04 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T21:34:50.749Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-04-02T20:55:03.121Z
+Commit: Resolve null pointer exception in user service
